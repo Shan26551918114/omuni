cc255006a7cf8b8aa732ac94c9bbecfc74fc4f60
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-08-20 17:11:01 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -61,22 +61,3 @@ canvas_result = st_canvas(
     key="canvas",
 )
 
-# Do something interesting with the image data and paths
-#if canvas_result.image_data is not None:
- #   st.image(canvas_result.image_data)
-import altair as alt
-import streamlit as st
-
-source = pd.DataFrame(np.cumsum(np.random.randn(100, 3), 0).round(2),
-                    columns=['alcohol', 'beer', 'coke'], index=pd.RangeIndex(100, name='x'))
-source = source.reset_index().melt('x', var_name='category', value_name='y')
-
-line_chart = alt.Chart(source).mark_line(interpolate='basis').encode(
-    alt.X('x', title='Year'),
-    alt.Y('y', title='Amount in liters'),
-    color='category:N'
-).properties(
-    title='Sales of consumer goods'
-)
-
-st.altair_chart(line_chart)
