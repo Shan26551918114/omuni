d0d9f3d184af84af88d62f72cf5d47aefa26def5
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-08-24 14:58:18 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -39,7 +39,7 @@ from streamlit_drawable_canvas import st_canvas
 # Specify canvas parameters in application
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+#bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
@@ -51,7 +51,7 @@ canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_color=bg_color,
+#    background_color=bg_color,
     
     background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=realtime_update,
