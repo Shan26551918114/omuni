7f4160b491defda7b17a6cf14ad342ff73ac73cb
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-08-25 10:49:58 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,34 +1,63 @@
 import streamlit as st
+import pandas as pd
 
-import matplotlib.pyplot as plt
+st.title("新潟人工知能研究所")
+st.write("""オムニ株式会社""")
 
-import numpy as np
+#number = st.slider("pick a number", 0 , 100)
 
-
-st.date_input('Date input')
-st.time_input('Time entry')
+#file = st.file_uploader("pick a file")
 
 from PIL import Image
 import numpy as np
+import streamlit as st
+
+# Function to Read and Manupilate Images
+def load_image(img):
+    im = Image.open(img)
+    image = np.array(im)
+    return image
+
+# Uploading the File to the Page
+#uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+
+# Checking the Format of the page
+#if uploadFile is not None:
+    # Perform your Manupilations (In my Case applying Filters)
+#    img = load_image(uploadFile)
+#    st.image(img)
+#    st.write("Image Uploaded Successfully")
+#else:
+#    st.write("Make sure you image is in JPG/PNG Format.")
+ #   st.write("画像の形式はJPG/PNGに対応しています")
 
 from streamlit_drawable_canvas import st_canvas
 
 # Specify canvas parameters in application
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 16, 3)
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-#bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("freedraw", "line", "rect", "transform")
+    "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
 )
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
+# Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
-    stroke_color=stroke_color, 
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    
     background_image=Image.open(bg_image) if bg_image else None,
-    height=400,
+    update_streamlit=realtime_update,
+    height=600,
     width=600,
     drawing_mode=drawing_mode,
     key="canvas",
 )
+
+# Do something interesting with the image data and paths
+#if canvas_result.image_data is not None:
+ #   st.image(canvas_result.image_data)
