b1141c7d161272cb642b783d1c7d41dac5b8e6b1
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-08-25 10:46:50 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,43 +1,16 @@
 import streamlit as st
-import pandas as pd
 
-st.title("新潟人工知能研究所")
-st.write("""オムニ株式会社""")
+import matplotlib.pyplot as plt
+
+import numpy as np
 
 
 st.date_input('Date input')
 st.time_input('Time entry')
 
-#number = st.slider("pick a number", 0 , 100)
-
-#file = st.file_uploader("pick a file")
-
 from PIL import Image
-import numpy as np 
-import streamlit as st 
+import numpy as np
 
-# Function to Read and Manupilate Images
-def load_image(img):
-    im = Image.open(img)
-    image = np.array(im)
-    return image
-
-# Uploading the File to the Page
-#uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
-
-# Checking the Format of the page
-#if uploadFile is not None:
-    # Perform your Manupilations (In my Case applying Filters)
-#    img = load_image(uploadFile)
-#    st.image(img)
-#    st.write("Image Uploaded Successfully")
-#else:
-#    st.write("Make sure you image is in JPG/PNG Format.")
- #   st.write("画像の形式はJPG/PNGに対応しています")
-
-import pandas as pd
-from PIL import Image
-import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
 # Specify canvas parameters in application
@@ -46,10 +19,9 @@ stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 #bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
+    "Drawing tool:", ("freedraw", "line", "rect", "transform")
 )
 
-# Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
@@ -62,4 +34,3 @@ canvas_result = st_canvas(
     drawing_mode=drawing_mode,
     key="canvas",
 )
-
