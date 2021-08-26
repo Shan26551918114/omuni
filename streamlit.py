f5a4c8588183b76e408f0c8c036f0b303a0dcc97
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-08-26 10:51:05 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,22 +4,11 @@ import pandas as pd
 st.title("新潟人工知能研究所")
 st.write("""オムニ株式会社""")
 
-#number = st.slider("pick a number", 0 , 100)
-
-#file = st.file_uploader("pick a file")
-
 from PIL import Image
 import numpy as np
 
-
-
-
-
-
-
 from streamlit_drawable_canvas import st_canvas
 
-# Specify canvas parameters in application
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 16, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
