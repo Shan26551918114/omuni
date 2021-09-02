6258344807deb1457a5c3fdd530a70d029e1f91c
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-09-02 10:50:25 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,52 @@
 import streamlit as st
 import pandas as pd
+import streamlit.components.v1 as components
+
+components.html("""<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.pill-nav a {
+  display: inline-block;
+  color: black;
+  text-align: center;
+  padding: 14px;
+  text-decoration: none;
+  font-size: 17px;
+  border-radius: 5px;
+}
+
+.pill-nav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.pill-nav a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+</style>
+</head>
+<body>
+<h2>新潟人工知能研究所</h2>
+
+
+<div class="pill-nav">
+  <a href="#home">Single Image</a>
+  <a href="#news">Multiple Images</a>
+  <a class="active" href="#contact">Drawing</a>
+  <a href="#about">Help</a>
+</div>
+
+</body>
+</html>
+
+
+""")
 
 st.title("新潟人工知能研究所")
 st.write("""オムニ株式会社""")
