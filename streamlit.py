849189a9c5f4b14fb60a4e40c7a47af8e82d4b4e
Shan26551918114 <89066485+Shan26551918114@users.noreply.github.com>
2021-09-09 11:04:52 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,28 +1,44 @@
 import streamlit as st
-import pandas as pd
-import streamlit.components.v1 as components
-
-
-
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
+import cv2 
+import sys
+
 
 from streamlit_drawable_canvas import st_canvas
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 16, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("freedraw", "line", "rect", "transform")
-)
+def main():
+    """Detection"""
+    
+    st.title("新潟人工知能研究所")
+    st.subheader("OMUNI APP")
+    
+    activities = ["Detection", "Paint", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activities)
+    
+    if choice == 'Detection':
+        st.text("Detection")
+        uploaded_file1 = st.file_uploader("Choose a file")
+        img_target=Image.open(uploaded_file1)
+        uploaded_file2 = st.file_uploader("Choose a file")
+        img_target=Image.open(uploaded_file2)
+        st.image(img_target)     
+    if choice == 'Paint':
+        st.text("Paint")
+        stroke_width = st.sidebar.slider("Stroke width", 1, 16, 3)
+        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+        drawing_mode = st.sidebar.selectbox("Drawing tool:", ("freedraw", "line", "rect", "circle", "transform"))
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",
+            stroke_width=stroke_width,
+            stroke_color=stroke_color,
+            background_image=Image.open(bg_image) if bg_image else None,
+            height=400,width=500,drawing_mode=drawing_mode,key="canvas",)
+        
 
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,    
-    background_image=Image.open(bg_image) if bg_image else None,
-    height=400,
-    width=600,
-    drawing_mode=drawing_mode,
-    key="canvas",
-)
+    elif choice == 'About':
+        st.text("About")
+
+if __name__ == "__main__":
+    main()
